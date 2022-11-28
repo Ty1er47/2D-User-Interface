4db0016fad0a701afdec79263909b1b036856cee
Ty1er47 <111007514+Ty1er47@users.noreply.github.com>
2022-11-28 10:37:27 -0800

    blah

--- 2dUserInterface/Assets/Scripts/Target.cs
+++ 2dUserInterface/Assets/Scripts/Target.cs
@@ -6,24 +6,20 @@ public class Target : MonoBehaviour
 {
     public int PointValue = 1;
     public float MinSpeed = 10;
-     public float MaxSpeed = 20;
-     public float MaxTorque = 10;
+    public float MaxSpeed = 20;
+    public float MaxTorque = 10;
 
-     private Rigidbody2D _targerRb;
-     private GameManager _gameManager;
+    private Rigidbody2D _targetRb;
+    private GameManager _gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        _targerRb = GetComponent<Rigidbody2D>();
-        _gameManager = gameObject.Find("Game Manager").GetComponent<GameManager>();
+        _targetRb = GetComponent<Rigidbody2D>();
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
-        _targerRb.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
-         _targerRb.AddTorque(RandomizeTorque());
-    }
-
-    private float RandomizeTorque()
-    {
-        throw new System.NotImplementedException();
+        _targetRb.AddForce(Vector2.up * RandomizeForce(), ForceMode2D.Impulse);
+        _targetRb.AddTorque(RandomizeTorque());
     }
 
     // Update is called once per frame
@@ -31,18 +27,32 @@ public class Target : MonoBehaviour
     {
         
     }
- private float RandomizeForce()
- {
-     return Random.Range(MinSpeed, MaxSpeed);
- }
 
-private void OnTriggerEnter2D(Collider2D other)
- {
-     Destroy(this.gameObject);
-     if(!other.gameObject.CompareTag("Bad"))
-     {
-         //Debug.Log("GameOver");
-         _gameManager.IsGameActive = false;
-     }
- }
- }
+    private float RandomizeForce()
+    {
+        return Random.Range(MinSpeed, MaxSpeed);
+    }
+
+    private float RandomizeTorque()
+    {
+        return Random.Range(-MaxTorque, MaxTorque);
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("You clicked on " + gameObject.name);
+        _gameManager.UpdateScore(PointValue);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+
+        if(!other.gameObject.CompareTag("Bad"))
+        {
+            //Debug.Log("Game Over");
+            _gameManager.IsGameActive = false;
+        }
+    }
+}
